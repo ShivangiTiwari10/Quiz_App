576a4eb1dedb1f7d9484e90bf789faacce5bf091
Shivangi <shivangit1111@gmail.com>
2023-06-28 19:58:13 +0530

    feat: bind view

--- app/src/main/java/com/pratyakshkhurana/quizapp/MainActivity.kt
+++ app/src/main/java/com/pratyakshkhurana/quizapp/MainActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.pratyakshkhurana.quizapp.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         buttonStart.setOnClickListener {
             if (enterNameEditText.text.toString().isEmpty()) {
